98c249c7c819a966e262c040aa8ca39353f7893b
HiranyaRanawaka99 <hiranya.ranawaka99@gmail.com>
2023-10-24 10:36:54 +0300

    add index and show

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,28 +2,30 @@
 
 @section('content')
 
-@foreach($projects as $project)
-<h1> {{ $project->title }} </h1>
+<div class="container mt-5">
+
+  @foreach($projects as $project)
+  <h1> {{ $project->title }} </h1>
 @endforeach
 
 <table class="table">
-    <thead>
-        <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Title</th>
-            <th scope="col"></th>
-            <th scope="col"></th>
-        </tr>
-    </thead>
-    <tbody>
-        @forelse($projects as $project)
-      <tr>
-        <th scope="row"> {{ $project->id}} </th>
-        <td> </td>
-        <td></td>
-        <td></td>
-      </tr>
-      @empty
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Title</th>
+      <th scope="col"></th>
+      <th scope="col">Dettagli</th>
+    </tr>
+  </thead>
+  <tbody>
+    @forelse($projects as $project)
+    <tr>
+      <th scope="row"> {{ $project->id}} </th>
+      <td> </td>
+      <td></td>
+      <td> <a href="{{ route('admin.projects.show', $project)}}" class=""> Show </a></td>
+    </tr>
+    @empty
       @endforelse
       <tr>
         <th scope="row"></th>
@@ -31,7 +33,7 @@
         <td></td>
         <td></td>
       </tr>
-
+      
       <tr>
         <th scope="row"></th>
         <td colspan="2"></td>
@@ -39,7 +41,8 @@
       </tr>
     </tbody>
   </table>
-
-
-
-@endsection
+  
+  
+  
+</div>
+  @endsection
